646346c34465661d10cb0b0e3d7bb323d01af091
moarme <mozer.arthur@me.com>
2018-07-14 10:06:46 +0300

    2

---
+++ EscapeRoom/Source/EscapeRoom/DoorOpener.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DoorOpener.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UDoorOpener::UDoorOpener()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UDoorOpener::BeginPlay()
+{
+	Super::BeginPlay();
+	//for (float i = 0; i < 90; i += 2) {
+		//GetOwner()->
+		AActor *p_owner = GetOwner();
+		FRotator rotation = FRotator(0.0f, 0.0f, 90.0f);
+		float z_rotate = p_owner->GetActorRotation().GetComponentForAxis(EAxis::Type::Z);
+		p_owner->SetActorRotation(*new FQuat(0.0f, 0.0f, z_rotate+2.0f,0.0f));
+	//}
+	
+	// ...
+	
+}
+
+
+// Called every frame
+void UDoorOpener::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
